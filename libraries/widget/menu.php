c5169c86ee6a4255c4843c5dc1d9618afaff312d
crynobone <crynobone@gmail.com>
2012-07-06 23:38:54 +0800

    Docblock explaination for Orchestra\Widget\Menu::render()

--- libraries/widget/menu.php
+++ libraries/widget/menu.php
@@ -2,6 +2,12 @@
 
 class Menu extends Driver
 {
-
+	/**
+	 * Render doesn't do anything at the moment but instead just
+	 * comply with the abstract class from Orchestra\Widget\Driver
+	 *
+	 * @access public
+	 * @return void
+	 */
 	public function render() {}
 }
