ea3224a9b68f38d5dc35cabc9a8a01189af4442b
crynobone <crynobone@gmail.com>
2012-07-23 23:41:47 +0800

    Base configuration for extension, this should allow Orchestra to overwrite configuration for extension.

---
+++ libraries/extension/configure.php
@@ -0,0 +1,10 @@
+<?php namespace Orchestra\Extension;
+
+use \Closure;
+
+class Configure
+{
+	public static function make($extension, Closure $callback) {}
+
+	protected function __construct() {}
+}
