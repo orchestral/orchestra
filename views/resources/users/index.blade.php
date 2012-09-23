7846eccf183bdf5cd6c209f423946c4edc394407
crynobone <crynobone@gmail.com>
2012-09-23 23:51:04 +0800

    Form search navbar updated to bootstrap 2.1 standard.

--- views/resources/users/index.blade.php
+++ views/resources/users/index.blade.php
@@ -8,20 +8,23 @@
 		<h2>{{ $page_name }}</h2>
 	</div>
 
-	{{ Form::open(handles('orchestra::users'), 'GET', array('class' => 'well form-inline')) }}
-		<div class="pull-left">
-			&nbsp;{{ Form::text('q', Input::get('q'), array('placeholder' => __('orchestra::label.search.keyword'), 'class' => 'input-xlarge'))}}
-		</div>
-		<div class="pull-left">
-			&nbsp;{{ Form::select('roles[]', $roles, Input::get('roles'), array('multiple' => true, 'placeholder' => __('orchestra::label.users.roles'), 'class' => 'input-xlarge'))}}
-		</div>
-		<div class="pull-left">
-			&nbsp;{{ Form::button(__('orchestra::label.search.button'), array('type' => 'submit', 'class' => 'btn btn-primary')) }}
-		</div>
+	<div class="navbar">
+		<div class="navbar-inner">
 
-	{{ Form::close() }}
-
-	<br>
+			{{ Form::open(handles('orchestra::users'), 'GET', array('class' => 'navbar-form')) }}
+				<div class="pull-left">
+					&nbsp;{{ Form::text('q', Input::get('q'), array('placeholder' => __('orchestra::label.search.keyword'), 'class' => 'input-xlarge'))}}
+				</div>
+				<div class="pull-left">
+					&nbsp;{{ Form::select('roles[]', $roles, Input::get('roles'), array('multiple' => true, 'placeholder' => __('orchestra::label.users.roles'), 'class' => 'input-xlarge'))}}
+				</div>
+				<div class="pull-left">
+					&nbsp;{{ Form::button(__('orchestra::label.search.button'), array('type' => 'submit', 'class' => 'btn btn-primary')) }}
+				</div>
+			{{ Form::close() }}
+		
+		</div>
+	</div>
 
 	{{ $table }}
 
