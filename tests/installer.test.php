8fa7d9216513247a45cb862bd95de21c5e920c42
crynobone <crynobone@gmail.com>
2012-10-27 23:51:40 +0800

    Add initial unit testing
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/installer.test.php
@@ -0,0 +1,24 @@
+<?php
+
+class TestInstaller extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * Setup the test
+	 */
+	public function setup()
+	{
+		Bundle::start('orchestra');
+
+		Orchestra\Installer::$status = false;
+	}
+
+	/**
+	 * Test Orchestra\Installer::installed()
+	 *
+	 * @test
+	 */
+	public function testStatus()
+	{
+		$this->assertFalse(Orchestra\Installer::installed());
+	}
+}
