257e89e3730664973932681f1a60d175b88c22c3
crynobone <crynobone@gmail.com>
2012-11-11 15:12:10 +0800

    Update Controller Test Case
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/controller.test.php
+++ tests/controller.test.php
@@ -1,13 +1,24 @@
 <?php
 
-class TestController extends PHPUnit_Framework_TestCase
-{
+class ControllerTest extends PHPUnit_Framework_TestCase {
+	
 	/**
-	 * Setup the test
+	 * Setup the test environment.
 	 */
 	public function setUp()
 	{
 		Bundle::start('orchestra');
+		$_SERVER['test.orchestra.started'] = null;
+		$_SERVER['test.orchestra.done'] = null;
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($_SERVER['test.orchestra.started']);
+		unset($_SERVER['test.orchestra.done']);
 	}
 	
 	/**
@@ -21,5 +32,40 @@ class TestController extends PHPUnit_Framework_TestCase
 
 		$this->assertTrue(\View::$shared['fluent_layout']);
 		$this->assertEquals(Orchestra\Core::memory(), \View::$shared['orchestra_memory']);
+	}
+
+	/**
+	 * Test Orchestra\Controller::__construct() triggers `orchestra.started: backend`
+	 *
+	 * @test
+	 */
+	public function testConstructTriggerEvents()
+	{
+		Event::listen('orchestra.started: backend', function()
+		{
+			$_SERVER['test.orchestra.started'] = 'foo';
+		});
+
+		$controller = new Orchestra\Controller;
+
+		$this->assertEquals('foo', $_SERVER['test.orchestra.started']);
+	}
+
+	/**
+	 * Test Orchestra\Controller::after() triggers `orchestra.done: backend`
+	 *
+	 * @test
+	 */
+	public function testAfterTriggerEvent()
+	{
+
+		Event::listen('orchestra.done: backend', function()
+		{
+			$_SERVER['test.orchestra.done'] = 'foo'; 
+		});
+
+		$controller = with(new Orchestra\Controller)->after('foobar');
+
+		$this->assertEquals('foo', $_SERVER['test.orchestra.done']);
 	}
 }
