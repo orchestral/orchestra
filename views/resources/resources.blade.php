81e8d124dc0e1fad722b43726e02960bbc510a4f
crynobone <crynobone@gmail.com>
2012-11-17 19:00:42 +0800

    Let resources own view take control of title or description, instead of prefixing from Orchestra template
    Signed-off-by: crynobone <crynobone@gmail.com>

--- views/resources/resources.blade.php
+++ views/resources/resources.blade.php
@@ -16,16 +16,6 @@
 		</ul>
 	</div>
 	<div class="span10">
-		@if ( ! empty($_title_))
-		<div class="page-header">
-			<h2>{{ ! empty($_title_) ? $_title_ : 'Something Awesome Without A Name' }} 
-				@if ( ! empty($_description_))
-				<small>{{ $_description_ ?: '' }}</small>
-				@endif
-			</h2>
-		</div>
-		@endif
-
 		{{ $content }}
 	</div>
 </div>
