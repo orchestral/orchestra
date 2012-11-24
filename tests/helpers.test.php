1c51565ba8e0b472e9f8718a30ebdb5d538b3f58
crynobone <crynobone@gmail.com>
2012-11-25 07:13:38 +0800

    Add helpers unit test.

---
+++ tests/helpers.test.php
@@ -0,0 +1,20 @@
+<?php
+
+class HelpersTest extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		Bundle::start('orchestra');
+	}
+
+	/**
+	 * Test handles() return proper URL
+	 */
+	public function testHandlesReturnProperURL()
+	{
+
+	}
+}
