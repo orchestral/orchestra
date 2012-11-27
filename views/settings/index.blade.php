a840a920e8f4bdf3d958efb18ce21691ce9c8ef6
crynobone <crynobone@gmail.com>
2012-11-27 15:58:09 +0800

    Improved e-mail configuration UX by adding some JavaScript event listener. (solved #60) Signed-off-by: crynobone <crynobone@gmail.com>

--- views/settings/index.blade.php
+++ views/settings/index.blade.php
@@ -21,4 +21,36 @@
 	</div>
 </div>
 
+<script>
+	jQuery(function onSettingPageReady ($) { 'use strict';
+		var ev, emailDefault;
+
+		ev           = Javie.Events.make();
+		emailDefault = $('select[name="email_default"]');
+
+		ev.listen('setting.changed: email.default', function (e, self) {
+			var value = self.value ? self.value : '';
+
+			$('input[name^="email_smtp"], input[name^="email_sendmail"]')
+				.parent().parent().hide();
+
+			switch (value) {
+				case 'smtp' :
+					$('input[name^="email_smtp"]').parent().parent().show();
+					break;
+				case 'sendmail' :
+					$('input[name^="email_sendmail"]').parent().parent().show();
+					break;
+			}
+		});
+
+		emailDefault.on('change', function onChangeEmailDefault (e) {
+			ev.fire('setting.changed: email.default', [e, this]);
+		});
+
+		// lets trigger an onChange event.
+		emailDefault.trigger('change');
+	});
+</script>
+
 @endsection
