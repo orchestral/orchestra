6ec5523c71ab8af1f1c6a99634362451391a501c
crynobone <crynobone@gmail.com>
2012-12-01 04:48:41 +0800

    Missing testcase on ExtensionPublisherTest class
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/extension/publisher.test.php
+++ tests/extension/publisher.test.php
@@ -9,4 +9,19 @@ class ExtensionPublisherTest extends PHPUnit_Framework_TestCase {
 	{
 		Bundle::start('orchestra');
 	}
+
+	/**
+	 * Test instance of Orchestra\Extension\Publisher default driver
+	 *
+	 * @test
+	 */
+	public function testInstanceOfDefaultDriver()
+	{
+		Orchestra\Core::memory()->put('orchestra.publisher.driver', 'ftp');
+
+		$this->assertInstanceOf('Orchestra\Extension\Publisher\Driver',	
+			Orchestra\Extension\Publisher::driver());
+		$this->assertInstanceOf('Orchestra\Extension\Publisher\FTP',	
+			Orchestra\Extension\Publisher::driver());
+	}
 }
