ef8123d1ad00b83f022afe5896feaa061176e149
crynobone <crynobone@gmail.com>
2012-12-09 01:26:23 +0800

    Basic Orchestra\Extension\Publisher\FTP testcase
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/extension/publisher/ftp.test.php
@@ -0,0 +1,51 @@
+<?php
+
+Bundle::start('orchestra');
+
+class ExtensionPublisherFTPTest extends Orchestra\Testable\TestCase {
+
+	/**
+	 * User instance.
+	 *
+	 * @var Orchestra\Model\User
+	 */
+	private $user = null;
+
+	/**
+	 * Stub instance.
+	 *
+	 * @var Orchestra\Extension\Publisher\FTP
+	 */
+	private $stub = null;
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->user = Orchestra\Model\User::find(1);
+		$this->stub = new Orchestra\Extension\Publisher\FTP;
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($this->user);
+		$this->be(null);
+
+		parent::tearDown();
+	}
+
+	/**
+	 * Test instanceof stub
+	 */
+	public function testInstanceOfStub()
+	{
+		$this->assertInstanceOf('Orchestra\Extension\Publisher\Driver', $this->stub);
+	}
+
+}
