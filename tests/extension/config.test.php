84ae34b50515f96b662b725fb35f40e12c878c8b
crynobone <crynobone@gmail.com>
2012-12-08 20:29:30 +0800

    Update additional test cases
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/extension/config.test.php
@@ -0,0 +1,35 @@
+<?php
+
+Bundle::start('orchestra');
+
+class ExtensionConfigTest extends Orchestra\Testable\TestCase {
+	
+	/**
+	 * User instance.
+	 *
+	 * @var Orchestra\Model\User
+	 */
+	private $user = null;
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->user = Orchestra\Model\User::find(1);
+		$this->be($this->user);
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($this->user);
+		$this->be(null);
+
+		parent::tearDown();
+	}
+}
--- tests/extension/publisher.test.php
+++ tests/extension/publisher.test.php
@@ -3,7 +3,47 @@
 Bundle::start('orchestra');
 
 class ExtensionPublisherTest extends Orchestra\Testable\TestCase {
-	
+
+	/**
+	 * Stub instance.
+	 *
+	 * @var PublisherStub
+	 */
+	private $stub = null;
+
+	/**
+	 * User instance.
+	 *
+	 * @var Orchestra\Model\User
+	 */
+	private $user = null;
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->stub = new PublisherStub;
+		$this->user = Orchestra\Model\User::find(1);
+
+		$this->be($this->user);
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($this->stub);
+		unset($this->user);
+
+		$this->be(null);
+
+		parent::tearDown();
+	}
+
 	/**
 	 * Test instance of Orchestra\Extension\Publisher default driver
 	 *
@@ -17,5 +57,67 @@ class ExtensionPublisherTest extends Orchestra\Testable\TestCase {
 			Orchestra\Extension\Publisher::driver());
 		$this->assertInstanceOf('Orchestra\Extension\Publisher\FTP',	
 			Orchestra\Extension\Publisher::driver());
+	}
+
+	/**
+	 * Test instance of Orchestra\Extension\Publisher using stub.
+	 *
+	 * @test
+	 */
+	public function testStubPublisher()
+	{
+		$this->assertInstanceOf('Orchestra\Extension\Publisher\Driver',	
+			$this->stub);
+
+		$this->assertTrue($this->stub->upload(DEFAULT_BUNDLE));
+		$this->assertTrue($this->stub->connected());
+	}
+}
+
+class PublisherStub extends Orchestra\Extension\Publisher\Driver {
+	/**
+	 * Get service connection instance.
+	 *
+	 * @access public
+	 * @return Object
+	 */
+	public function connection()
+	{
+		return $this;
+	}
+
+	/**
+	 * Connect to the service.
+	 *
+	 * @access public	
+	 * @param  array    $config
+	 * @return void
+	 */
+	public function connect($config = array())
+	{
+		// connect to foo.... connected.
+	}
+
+	/**
+	 * Upload the file.
+	 *
+	 * @access public
+	 * @param  string   $name   Extension name
+	 * @return bool
+	 */
+	public function upload($name)
+	{
+		return true;
+	}
+
+	/**
+	 * Verify that the driver is connected to a service.
+	 *
+	 * @access public
+	 * @return bool
+	 */
+	public function connected()
+	{
+		return true;
 	}
 }
