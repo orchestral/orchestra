2db99045061baff4475ad2e24b803e52ebd22052
crynobone <crynobone@gmail.com>
2012-12-19 16:29:55 +0800

    Add default role into Orchestra Platform, previously in authorize extension.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ config/orchestra.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	/*
+	|----------------------------------------------------------------------
+	| Default Role
+	|----------------------------------------------------------------------
+	| 
+	| The default role can't be deleted at any cause and would always have
+	| Orchestra `manage-user` and `manage-orchestra` actions.
+	*/
+	'default_role' => 1
+);
