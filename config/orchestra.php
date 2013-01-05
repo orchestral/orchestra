e37a257d3e0e8a7c6630b135b1921209710aa5c1
crynobone <crynobone@gmail.com>
2013-01-05 09:42:58 +0800

    [#83] Allow member_role to be configurable
    Signed-off-by: crynobone <crynobone@gmail.com>

--- config/orchestra.php
+++ config/orchestra.php
@@ -8,6 +8,19 @@ return array(
 	| 
 	| The default role can't be deleted at any cause and would always have
 	| Orchestra `manage-user` and `manage-orchestra` actions.
+	|
 	*/
-	'default_role' => 1
+
+	'default_role' => 1,
+
+	/*
+	|----------------------------------------------------------------------
+	| Default Member Role
+	|----------------------------------------------------------------------
+	| 
+	| The default member role.
+	|
+	*/
+
+	'default_member_role' => 2,
 );
