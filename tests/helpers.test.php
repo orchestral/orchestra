f06a41a710ab9d4b8d28f5a5d7174fa6748076e3
crynobone <crynobone@gmail.com>
2013-01-14 19:55:47 +0800

    Update locate test cases
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/helpers.test.php
+++ tests/helpers.test.php
@@ -39,8 +39,14 @@ class HelpersTest extends Orchestra\Testable\TestCase {
 	 */
 	public function testLocateReturnProperViewPath()
 	{
+		$theme = Bundle::path('orchestra').'tests'.DS.'fixtures'.DS.'public'.DS.'themes'.DS;
 		$view1 = locate('path: /path/to/somewhere');
+		$view2 = locate('home.index');
+		$view3 = locate('error.404');
 
 		$this->assertEquals('path: /path/to/somewhere', $view1);
+		$this->assertEquals("path: {$theme}default/home/index.blade.php", $view1);
+		$this->assertEquals('error.404', $view3);
+
 	}
 }
