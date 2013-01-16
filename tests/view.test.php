6a109367dfa0159a038ceff90b9944d6f69eeafb
crynobone <crynobone@gmail.com>
2013-01-16 14:53:25 +0800

    Get view from reflectionobject
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/view.test.php
+++ tests/view.test.php
@@ -5,15 +5,45 @@ Bundle::start('orchestra');
 class ViewTest extends PHPUnit_Framework_TestCase {
 	
 	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		$_SERVER['view.started'] = null;
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($_SERVER['view.started']);
+	}
+
+	/**
 	 * Test instanceof Orchestra\View
 	 *
 	 * @test
 	 */
-	public function testInstanceOf()
+	public function testBasicView()
 	{
+		Event::listen('orchestra.started: view', function ()
+		{
+			$_SERVER['view.started'] = 'foo';
+		});
+
+		$this->assertTrue(is_null($_SERVER['view.started']));
+
 		$view = new Orchestra\View('home.index');
 
 		$this->assertInstanceOf('Laravel\View', $view);
 		$this->assertEquals('frontend', Orchestra\View::$theme);
+		$this->assertEquals('foo', $_SERVER['view.started']);
+
+		$refl = new \ReflectionObject($view);
+		$file = $ref->getProperty('view');
+		$file->setAccessible(true);
+
+		$this->assertEquals('home.index', $file->getValue($view));
 	}
 }
