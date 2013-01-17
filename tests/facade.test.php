a664687ac7be905cace32b395c9036b03b2350b0
crynobone <crynobone@gmail.com>
2013-01-17 09:43:25 +0800

    Add Facade test case
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ tests/facade.test.php
@@ -0,0 +1,19 @@
+<?php
+
+Bundle::start('orchestra');
+
+class FacadeTest extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * Test Orchestra::VERSION
+	 *
+	 * @test
+	 */
+	public function testVersionSyntax()
+	{
+		$version = Orchestra::VERSION;
+
+		$this->assertRegExp('/(\d{1,5})\.(\d{1,5})\.(\d{1,5})(\-[a|b][\d{1,5}])?', 
+			$version);
+	}
+}
