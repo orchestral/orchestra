8fb792d2338fe479ece9abeb977c9c0d9e5fe297
crynobone <crynobone@gmail.com>
2013-01-20 17:04:30 +0800

    Add ::user() and ::name() helper for Orchestra\Model\User\Meta model
    Signed-off-by: crynobone <crynobone@gmail.com>

--- models/user/meta.php
+++ models/user/meta.php
@@ -22,4 +22,32 @@ class Meta extends Eloquent {
 		return $this->belongs_to('Orchestra\Model\User', 'user_id');
 	}
 
+	/**
+	 * Get all meta data belong to a user.
+	 *
+	 * @static
+	 * @access public
+	 * @param  int      $user_id
+	 * @return Orchestra\Model\User\Meta
+	 */
+	public function user($user_id)
+	{
+		return static::where('user_id', '=', $user_id);
+	}
+
+	/**
+	 * Retun a meta data belong to a user.
+	 * 
+	 * @static
+	 * @access public
+	 * @param  string   $name
+	 * @param  int      $user_id
+	 * @return Orchestra\Model\User\Meta
+	 */
+	public static function name($name, $user_id)
+	{
+		return static::user($user_id)
+			->where('name', '=', $name)
+			->first();
+	}
 }
