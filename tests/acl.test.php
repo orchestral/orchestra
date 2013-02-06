b29beadd1efde5d14f20d0368c68409474077d5e
crynobone <crynobone@gmail.com>
2013-02-06 17:45:17 +0800

    small improvement to acl unit test.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- tests/acl.test.php
+++ tests/acl.test.php
@@ -5,6 +5,35 @@ Bundle::start('orchestra');
 class AclTest extends Orchestra\Testable\TestCase {
 
 	/**
+	 * Role instance.
+	 *
+	 * @var Orchestra\Model\Role
+	 */
+	protected $role = null;
+
+	/**
+	 * Setup the test environment.
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->role = Orchestra\Model\Role::create(array(
+			'name' => 'foo',
+		));
+	}
+
+	/**
+	 * Teardown the test environment.
+	 */
+	public function tearDown()
+	{
+		unset($this->role);
+
+		parent::tearDown();
+	}
+
+	/**
 	 * Test instanceof Orchestra\Acl
 	 *
 	 * @test
@@ -16,40 +45,66 @@ class AclTest extends Orchestra\Testable\TestCase {
 	}
 
 	/**
-	 * Test model CRUD for Orchestra\Model\Role does sync with Orchestra\Acl
+	 * Test model create CRUD for Orchestra\Model\Role does sync with Orchestra\Acl
 	 *
 	 * @test
 	 */
-	public function testModelRoleCRUDSyncWithAcl()
+	public function testModelCreateRoleCRUDSyncWithAcl()
 	{
-		$foo = Orchestra\Model\Role::create(array('name' => 'foo'));
-		$acl = Orchestra\Core::acl();
-
-		$this->assertTrue($acl->has_role($foo->name));
-
-		$refl = new \ReflectionObject($acl);
+		$acl   = Orchestra\Core::acl();
+		$refl  = new \ReflectionObject($acl);
 		$roles = $refl->getProperty('roles');
 		$roles->setAccessible(true);
 
+		$this->assertTrue($acl->has_role($this->role->name));
 		$this->assertEquals(array('guest', 'member', 'administrator', 'foo'),
 			$roles->getValue($acl)->get());
+	}
 
-		$foo->name = 'foobar';
-		$foo->save();
+	/**
+	 * Test model update CRUD for Orchestra\Model\Role does sync with Orchestra\Acl
+	 *
+	 * @test
+	 */
+	public function testModelUpdateRoleCRUDSyncWithAcl()
+	{
+		$acl   = Orchestra\Core::acl();
+		$refl  = new \ReflectionObject($acl);
+		$roles = $refl->getProperty('roles');
+		$roles->setAccessible(true);
 
-		$this->assertTrue($acl->has_role($foo->name));
+		$this->role->name = 'foobar';
+		$this->role->save();
+
+		$this->assertTrue($acl->has_role($this->role->name));
 		$this->assertEquals(array('guest', 'member', 'administrator', 'foobar'),
 			$roles->getValue($acl)->get());
+
+		$this->role->name = 'foo';
+		$this->role->save();
+	}
+
+	/**
+	 * Test model delete CRUD for Orchestra\Model\Role does sync with Orchestra\Acl
+	 *
+	 * @test
+	 */
+	public function testModelDeleteRoleCRUDSyncWithAcl()
+	{
+		$acl   = Orchestra\Core::acl();
+		$refl  = new \ReflectionObject($acl);
+		$roles = $refl->getProperty('roles');
+		$roles->setAccessible(true);
 
 		$editor = Orchestra\Model\Role::create(array('name' => 'editor'));
 
 		$this->assertTrue($acl->has_role($editor->name));
-		$this->assertEquals(array('guest', 'member', 'administrator', 'foobar', 'editor'),
+		$this->assertEquals(array('guest', 'member', 'administrator', 'foo', 'editor'),
 			$roles->getValue($acl)->get());
 
-		$foo->delete();
+		$this->role->delete();
 
-		$this->assertFalse($acl->has_role('foobar'));
+		$this->assertFalse($acl->has_role('foo'));
 		$this->assertEquals(array('guest', 'member', 'administrator', 4 => 'editor'),
 			$roles->getValue($acl)->get());
 	}
