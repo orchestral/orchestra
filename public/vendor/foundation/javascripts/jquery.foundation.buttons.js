b915413741de37ce9a017be25daf40cee2263a43
crynobone <crynobone@gmail.com>
2013-02-27 12:51:11 +0800

    Update Zurb Foundation v3.2.5
    Signed-off-by: crynobone <crynobone@gmail.com>

--- public/vendor/foundation/javascripts/jquery.foundation.buttons.js
+++ public/vendor/foundation/javascripts/jquery.foundation.buttons.js
@@ -3,17 +3,19 @@
 
   $.fn.foundationButtons = function (options) {
     var $doc = $(document),
-      config = $.extend({
-        dropdownAsToggle:true,
-        activeClass:'active'
-      }, options),
+        config = $.extend({
+          dropdownAsToggle:false,
+          activeClass:'active'
+        }, options),
 
     // close all dropdowns except for the dropdown passed
       closeDropdowns = function (dropdown) {
+        // alert(dropdown.html());
         $('.button.dropdown').find('ul').not(dropdown).removeClass('show-dropdown');
       },
     // reset all toggle states except for the button passed
       resetToggles = function (button) {
+        // alert(button.html());
         var buttons = $('.button.dropdown').not(button);
         buttons.add($('> span.' + config.activeClass, buttons)).removeClass(config.activeClass);
       };
@@ -28,22 +30,24 @@
     // reset other active states
     $doc.on('click.fndtn', '.button.dropdown:not(.split), .button.dropdown.split span', function (e) {
       var $el = $(this),
-        button = $el.closest('.button.dropdown'),
-        dropdown = $('> ul', button);
-
-        // If the click is registered on an actual link then do not preventDefault which stops the browser from following the link
-        if (e.target.nodeName !== "A"){
+          button = $el.closest('.button.dropdown'),
+          dropdown = $('> ul', button);
+          
+        // If the click is registered on an actual link or on button element then do not preventDefault which stops the browser from following the link
+        if ($.inArray(e.target.nodeName, ['A', 'BUTTON'])){
           e.preventDefault();
         }
 
       // close other dropdowns
-      closeDropdowns(config.dropdownAsToggle ? dropdown : '');
-      dropdown.toggleClass('show-dropdown');
+      setTimeout(function () {
+        closeDropdowns(config.dropdownAsToggle ? '' : dropdown);
+        dropdown.toggleClass('show-dropdown');
 
-      if (config.dropdownAsToggle) {
-        resetToggles(button);
-        $el.toggleClass(config.activeClass);
-      }
+        if (config.dropdownAsToggle) {
+          resetToggles(button);
+          $el.toggleClass(config.activeClass);
+        }
+      }, 0);
     });
 
     // close all dropdowns and deactivate all buttons
@@ -59,10 +63,10 @@
     });
 
     // Positioning the Flyout List
-    var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny)', this).outerHeight() - 1,
-        largeButtonHeight   = $('.button.large.dropdown', this).outerHeight() - 1,
-        smallButtonHeight   = $('.button.small.dropdown', this).outerHeight() - 1,
-        tinyButtonHeight    = $('.button.tiny.dropdown', this).outerHeight() - 1;
+    var normalButtonHeight  = $('.button.dropdown:not(.large):not(.small):not(.tiny):visible', this).outerHeight() - 1,
+        largeButtonHeight   = $('.button.large.dropdown:visible', this).outerHeight() - 1,
+        smallButtonHeight   = $('.button.small.dropdown:visible', this).outerHeight() - 1,
+        tinyButtonHeight    = $('.button.tiny.dropdown:visible', this).outerHeight() - 1;
 
     $('.button.dropdown:not(.large):not(.small):not(.tiny) > ul', this).css('top', normalButtonHeight);
     $('.button.dropdown.large > ul', this).css('top', largeButtonHeight);
