0ecbd60d63b56bda722b23b5184a8aa679d81aac
crynobone <crynobone@gmail.com>
2013-03-01 17:23:14 +0800

    [#137] Allow user data to be escape on output.

--- views/support/table/horizontal.php
+++ views/support/table/horizontal.php
@@ -2,7 +2,9 @@
 	<thead>
 		<tr>
 <?php foreach ($columns as $col): ?>
-			<th<?php echo HTML::attributes($col->label_attributes ?: array()); ?>><?php echo $col->label; ?></th>
+			<th<?php echo HTML::attributes($col->label_attributes ?: array()); ?>>
+				<?php echo $col->label; ?>
+			</th>
 <?php endforeach; ?>
 		</tr>
 	</thead>
@@ -10,12 +12,19 @@
 <?php foreach ($rows as $row): ?>
 		<tr<?php echo HTML::attributes(call_user_func($row_attributes, $row) ?: array()); ?>>
 <?php foreach ($columns as $col): ?>
-			<td<?php echo HTML::attributes(call_user_func($col->cell_attributes, $row)); ?>><?php echo call_user_func($col->value, $row); ?></td>
+			<td<?php echo HTML::attributes(call_user_func($col->cell_attributes, $row)); ?>>
+				<?php 
+
+				$col_value = call_user_func($col->value, $row);
+				echo ( !! $col->escape ? e($col_value) : $col_value); ?>
+			</td>
 <?php endforeach; ?>
 		</tr>
 <?php endforeach; if ( ! count($rows) and $empty_message): ?>
 		<tr class="norecords">
-			<td colspan="<?php echo count($columns); ?>"><?php echo $empty_message->get(null, 'No records'); ?></td>
+			<td colspan="<?php echo count($columns); ?>">
+				<?php echo $empty_message->get(null, 'No records'); ?>
+			</td>
 		</tr>
 <?php endif; ?>
 	</tbody>
--- views/support/table/vertical.php
+++ views/support/table/vertical.php
@@ -2,9 +2,16 @@
 	<tbody>
 <?php foreach ($columns as $col): ?>
 		<tr>
-			<th<?php echo HTML::attributes($col->label_attributes ?: array()); ?>><?php echo $col->label; ?></th>
+			<th<?php echo HTML::attributes($col->label_attributes ?: array()); ?>>
+				<?php echo $col->label; ?>
+			</th>
 <?php foreach ($rows as $row): ?>
-			<td<?php echo HTML::attributes(call_user_func($col->cell_attributes, $row)); ?>><?php echo call_user_func($col->value, $row); ?></td>
+			<td<?php echo HTML::attributes(call_user_func($col->cell_attributes, $row)); ?>>
+				<?php 
+
+				$col_value = call_user_func($col->value, $row);
+				echo ( !! $col->escape ? e($col_value) : $col_value); ?>
+			</td>
 <?php endforeach; ?>
 		</tr>
 <?php endforeach; ?>
