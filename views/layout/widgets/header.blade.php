959415548f25281cc6319386539e68bbf501697b
crynobone <crynobone@gmail.com>
2013-03-05 17:05:23 +0800

    Allow to include add button

--- views/layout/widgets/header.blade.php
+++ views/layout/widgets/header.blade.php
@@ -4,6 +4,12 @@ $title       = Orchestra\Site::get('title');
 $description = Orchestra\Site::get('description'); ?>
 
 <div class="page-header">
+	@if (Orchestra\Site::get('header::add-button'))
+	<div class="pull-right">
+		<a href="{{ URL::current() }}/view" class="btn btn-primary">Add</a>
+	</div>
+	@endif
+	
 	<h2>{{ $title ?: 'Something Awesome' }}
 		@if ( ! empty($description))
 		<small>{{ $description ?: '' }}</small>
