e48b62ff506d54a7a8cd99e04a25d35a4958936f
crynobone <crynobone@gmail.com>
2013-03-27 19:17:01 +0800

    Add assertion helper and use 755 for fixtures/storage folder.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- libraries/testable/testcase.php
+++ libraries/testable/testcase.php
@@ -146,4 +146,83 @@ abstract class TestCase extends PHPUnit_Framework_TestCase {
 		$this->removeApplication();
 		$this->createApplication();
 	}
+
+	/**
+	 * Assert request has 200 response.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function assertResponseOk()
+	{
+		$this->assertResponseIs(200);
+	}
+
+	/**
+	 * Assert request has 404 response.
+	 *
+	 * @access public
+	 * @param  string   $redirect
+	 * @return void
+	 */
+	public function assertResponseNotFound()
+	{
+		$this->assertResponseIs(404);
+	}
+
+	/**
+	 * Assert request has response with selected status.
+	 *
+	 * @access public
+	 * @param  string   $status
+	 * @return void
+	 */
+	public function assertResponseIs($status = 200)
+	{
+		$response = $this->client->response;
+		$this->assertInstanceOf('\Laravel\Response', $response);
+		$this->assertEquals($status, $response->foundation->getStatusCode());
+	}
+
+	/**
+	 * Assert view is added.
+	 *
+	 * @access public
+	 * @param  string   $view
+	 * @param  integer  $status
+	 * @return void
+	 */
+	public function assertViewIs($view, $status = 200)
+	{
+		$response = $this->client->response;
+		$this->assertResponseIs($status);
+		$this->assertEquals($view, $response->content->view);
+	}
+
+	/**
+	 * Assert request is redirected.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function assertRedirected()
+	{
+		$response = $this->client->response;
+		$this->assertInstanceOf('\Laravel\Redirect', $response);
+		$this->assertEquals(302, $response->foundation->getStatusCode());
+	}
+
+	/**
+	 * Assert request is redirected to.
+	 *
+	 * @access public
+	 * @param  string   $redirect
+	 * @return void
+	 */
+	public function assertRedirectedTo($redirect)
+	{
+		$response = $this->client->response;
+		$this->assertRedirected();
+		$this->assertEquals($redirect, $response->foundation->headers->get('location'));
+	}
 }
